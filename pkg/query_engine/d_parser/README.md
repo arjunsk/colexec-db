ba099772d3fb2f982eb3062b55e2ec2571906c1c
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-06 00:50:08 -0700

    Parser

---
+++ pkg/query_engine/d_parser/README.md
@@ -0,0 +1,3 @@
+Refer here: https://github.com/myrp556/yacc_sql/tree/main
+
+https://github.com/blastrain/vitess-sqlparser
--- pkg/query_engine/d_parser/parser.go
+++ pkg/query_engine/d_parser/parser.go
@@ -5,6 +5,8 @@ import (
 	"github.com/blastrain/vitess-sqlparser/tidbparser/parser"
 )
 
+type Statement = ast.Statement
+
 func Parse(sql string) (ast.StmtNode, error) {
 	p := parser.New()
 	return p.ParseOneStmt(sql, "", "")
