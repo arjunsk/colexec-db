78ff4c34c72d8b8a73d95df0311a0b83c7eee920
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2023-09-08 10:49:42 -0700

    Added ToString() for vector and batch

---
+++ pkg/query_engine/l_vectorize/vmath/math.go
@@ -0,0 +1,15 @@
+package vmath
+
+import (
+	types "colexecdb/pkg/query_engine/a_types"
+	"math"
+)
+
+func Sqrt[T types.FixedSizeT](in []T) (out []int64) {
+	out = make([]int64, len(in))
+
+	for i, item := range in {
+		out[i] = int64(math.Sqrt(float64(item)))
+	}
+	return out
+}
